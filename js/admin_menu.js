1eaeb766a89f7e3b273c1dbd838999d89ecfb2f3
rania <raniagml12@gmail.com>
2017-12-18 19:27:01 +0200

    user interface

---
+++ js/admin_menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#add_button').click(function(){
+		$('#hidden_add').show();
+	});
+	$('#close_add_button').click(function(){
+		$('#hidden_add').hide();
+	});
+});
