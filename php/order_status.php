0b1c5c430349bd11c6a2b961bd24c0443c141d22
MaryamAbdelmoneam <maryam.m.abdelmoneam@gmail.com>
2017-12-20 06:56:25 +0200

    Add files via upload

---
+++ php/order_status.php
@@ -0,0 +1,29 @@
+<?php
+
+require('db_connect.php');
+	$conn=db_connect();
+		
+	$sql="SELECT `status` FROM `order` WHERE `ID`=".$_POST['order_number'];
+	$result = mysqli_query($conn, $sql);
+	
+	$i=0;
+	$Result;
+	if (mysqli_num_rows($result) > 0) {
+
+	   	while($row = mysqli_fetch_array($result)){
+
+			$Result=$rows['status'];
+		}
+	}
+	if ($Result==1)
+		$i="pending";
+	elseif ($Result==2)
+		$i="processed";
+	if ($Result==3)
+		$i="On the way";
+	else
+		$i="delivered";
+	
+	alert("Your Order is ",$i);	
+	
+?>
